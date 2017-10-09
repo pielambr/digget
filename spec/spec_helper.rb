80595bff38c31894efe3a3f02e7fd4e38f953c27
Pieterjan <pieterjan@rial.to>
2017-10-09 22:10:13 +0200

    Exclude spec directory from testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,16 @@
+if ENV['TRAVIS']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter 'spec/'
+  end
+
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../dummy/config/environment", __FILE__)
 require 'rspec/rails'
 require 'digget/validator'
 require 'i18n'
 require 'byebug'
-
-require 'simplecov'
-SimpleCov.start
-
-require 'codecov'
-SimpleCov.formatter = SimpleCov::Formatter::Codecov
